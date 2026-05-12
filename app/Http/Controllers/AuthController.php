6ef6568855af5c3c8cf10583a6e2ef4eb1d4e1df
Francis <brakay1136@gmail.com>
2026-05-12 08:19:53 -0800

    register, login and logout functions or methods working

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
 
 class AuthController extends Controller
 {
@@ -13,21 +17,64 @@ class AuthController extends Controller
     }
 
   
-    public function register()
+    public function register(Request $request)
     {
-        //
+        $request->validate([
+            'name' => 'required|min:5',
+
+            'email' => 'required|email|unique:users,email',
+
+            'password' => 'required|min:8|confirmed',
+
+            'role' => 'required'
+        ]);
+            User::create([
+                'name' => $request->name,
+
+                'email' => $request->email,
+
+                'password' => Hash::make($request->password),
+
+                'role' => $request->role
+            ]);
+
+            return redirect('/auth');
+
     }
 
     
-    public function login()
-    {
-        //
+   public function login(Request $request)
+{
+        // 1. validate input
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        // 2. attempt login
+        if (Auth::attempt($credentials)) {
+
+            // 3. regenerate session (security best practice)
+            $request->session()->regenerate();
+
+            return redirect('/dashboard');
+        }
+
+        // 4. if login fails
+        return back()->withErrors([
+            'email' => 'Invalid credentials'
+        ]);
     }
 
    
-    public function logout()
+    public function logout(Request $request)
     {
-        //
+        Auth::logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect('/auth');
     }
 
    
--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,12 +13,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+
             $table->string('name');
+
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+
             $table->string('password');
-            $table->string('role')->default('parent');
-            $table->rememberToken();
+
+            $table->enum('role', ['admin', 'teacher', 'parent']);
+
             $table->timestamps();
         });
 
