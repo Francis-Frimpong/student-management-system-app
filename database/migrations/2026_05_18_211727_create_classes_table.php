016d1ef80f4364efbf73c5e851440d7d30432e41
Francis <brakay1136@gmail.com>
2026-05-18 13:38:36 -0800

    all models and classes schema created

--- database/migrations/2026_05_18_211727_create_classes_table.php
+++ database/migrations/2026_05_18_211727_create_classes_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('classes', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 100);
+            $table->foreignId('teacher_id')
+                ->nullable()
+                ->constrained('users')
+                ->nullOnDelete();
+                
             $table->timestamps();
         });
     }
