1797e285b6a67d648df9d27fcdb1e54294b9a333
Francis <brakay1136@gmail.com>
2026-05-24 14:02:23 -0800

    minor ui changes made in the add student form

--- resources/views/admin/addstudents.blade.php
+++ resources/views/admin/addstudents.blade.php
@@ -3,6 +3,49 @@
         Add students
     </x-slot:title>
 
-      
+        <div class="container d-flex justify-content-center align-items-center vh-100">
+
+        <div class="auth-card card shadow p-4" style="width: 450px;">
+
+            <form method="POST" action="{{ route('admin.addclass.storeclass') }}">
+                @csrf
+
+                <h3 class="text-center mb-4">Add Student</h3>
+
+                <!-- Class Name -->
+                <div class="mb-3">
+                    <label class="form-label">Student Name</label>
+                    <input type="text" class="form-control" name="name" placeholder="Enter class name">
+                </div>
+
+                <!-- Assign Teacher -->
+                <div class="mb-4">
+                    <label class="form-label">Assign Class</label>
+
+                    <select class="form-select" name="class_id">
+                        <option value="">-- Select Class --</option>
+                      
+                       
+                    </select>
+                </div>
+                <div class="mb-4">
+                    <label class="form-label">Choose Parent</label>
+
+                    <select class="form-select" name="user_id">
+                        <option value="">-- Select Parent --</option>
+
+                       
+                    </select>
+                </div>
+
+                <button type="submit" class="btn btn-primary w-100">
+                    Add Class
+                </button>
+
+            </form>
+
+        </div>
+
+    </div>
 
 </x-adminlayout>
