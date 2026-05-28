8d983857e0e6a705bcb06cc20f1a7db647e13a32
Francis <brakay1136@gmail.com>
2026-05-28 11:11:54 -0800

    Assingment page UI improved

--- resources/views/teacher/assignments.blade.php
+++ resources/views/teacher/assignments.blade.php
@@ -9,9 +9,19 @@
 
     <div class="card p-3 mb-3">
       <h5>Create Assignment</h5>
-      <input type="text" class="form-control mb-2" placeholder="Title">
-      <textarea class="form-control mb-2" placeholder="Description"></textarea>
-      <button class="btn btn-primary">Add</button>
+      <form method="POST">
+        <input type="text" class="form-control mb-2" placeholder="Title">
+        <textarea class="form-control mb-2" placeholder="Description"></textarea>
+
+        <select name="class_id" class="form-control mb-2">
+          <option >--- Select a Class ---</option>
+          <option value="">Class 1</option>
+          <option value="">Class 2</option>
+          <option value="">Class 3</option>
+        </select>
+        <button class="btn btn-primary">Add Assignment</button>
+
+      </form>
     </div>
 
     <div class="card p-3">
